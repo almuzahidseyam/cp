65851fea280df0d50f7b78320558584aa975980b
AL-Muzahid <62498726+AL-Muzahid@users.noreply.github.com>
2021-04-16 03:36:30 +0600

    Update Pair_2.cpp

--- Pair_2.cpp
+++ Pair_2.cpp
@@ -1,4 +1,5 @@
 // https://codeforces.com/problemset/problem/166/A
+/*
 #include<bits/stdc++.h>
 #define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
@@ -20,4 +21,58 @@ int main()
     for(auto u: v)cout <<u.first << " " << u.second << endl;
     return 0;
 }
+*/
 
+
+/// Solve Way - 01
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+using namespace std;
+bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+    if(p1.first>p2.first)return 1;
+    if(p1.first==p2.first) return (p1.second < p2.second);
+    return 0;
+}
+int main()
+{
+    optimize();
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int,int>>v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(v.begin(), v.end(), cmp);
+    int ans = 0;
+    for(auto u: v){
+        if(u == v[k-1])++ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
+
+/// Solve Way - 02
+#include<bits/stdc++.h>
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+using namespace std;
+int main()
+{
+    optimize();
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int,int>>v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i].first >> v[i].second;
+        v[i].first *= -1;
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for(auto u: v){
+        if(u == v[k-1]) ++ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
